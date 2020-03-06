328496938642f7329f60620ff8403bfe2008de5d
MillerMC96 <pywang@ucdavis.edu>
2020-03-05 21:04:17 -0800

    correction

--- distances/plotting.m
+++ distances/plotting.m
@@ -1,5 +1,5 @@
-coor_file1 = fopen("dist_vs_time.txt");
-coor_file2 = fopen("dist_vs_time_inside.txt");
+coor_file1 = fopen("pulled_out2_short.txt");
+coor_file2 = fopen("pulled_out3_short.txt");
 coor_file3 = fopen("long_axis_entrance.txt");
 coor_file4 = fopen("long_axis_inside.txt");
 size_dist = [2 Inf];
@@ -7,8 +7,8 @@ size_dist = [2 Inf];
 default_dist = 6.525666667;%default distance in Angstroms
 distance = fscanf(coor_file1, '%d %f', size_dist);
 temp = fscanf(coor_file2, '%d %f', size_dist);
-frames = length(temp(1, :));
-for i = 1:frames
+frames = length(distance(1, :));
+for i = 1:30
     temp(1, i) = temp(1, i) + frames;
 end
 distance = [distance, temp];
@@ -22,7 +22,7 @@ xlabel('Time [ps]')
 ylim([5, 13])
 plot(distance(1, :), distance(2, :), 'b.')
 plot(distance(1, :), move_mean, 'r-', 'LineWidth', 2)
-line([0, 600],[default_dist, default_dist], 'Color', 'Black',...
+line([0, 100],[default_dist, default_dist], 'Color', 'Black',...
     'LineStyle', '--')
 legend('distance', 'moving average over 10 ps', 'crystal structure',...
      'Location','Southeast')
@@ -39,7 +39,7 @@ distance = [distance, temp];
 move_mean = movmean(distance(2, :), 10);
 figure
 hold
-title('Distance between ALA243 and ALA29 over time (long axis)')
+title('Distance between ALA242 and ALA29 over time (long axis)')
 ylabel('Distance [Å]')
 xlabel('Time [ps]')
 ylim([9, 16])
