524f2d8348961d14548fc49a139b3916d06c0f1d
MillerMC96 <pywang@ucdavis.edu>
2020-03-24 16:02:47 -0700

    removed first and last lines

--- src/distance_plot.py
+++ src/distance_plot.py
@@ -32,7 +32,7 @@ plt.scatter(time, dist, s = 2)
 plt.hlines(6.525666667, time[0], time[-1], colors = 'k', linestyles = '--', label = "crystal structure")
 
 #plotting moving mean
-plt.plot(time, move_mean, 'r', label = "moving average over " + str(N) + " ps")
+plt.plot(time[N-1:-1], move_mean[N-1:-1], 'r', label = "moving average over " + str(N) + " ps")
 
 #plotting moving std
 dist_upper_bound = list()
@@ -42,7 +42,7 @@ for dist_point, std in zip(move_mean, move_std):
     dist_upper_bound.append(dist_point + std)
     dist_lower_bound.append(dist_point - std)
 
-plt.fill_between(time, dist_upper_bound, dist_lower_bound, alpha = 0.4, label = "error band")
+plt.fill_between(time[N-1:-1], dist_upper_bound[N-1:-1], dist_lower_bound[N-1:-1], alpha = 0.4, label = "error band")
 
 plt.ylim(3, 13)
 plt.xlabel("time [ps]")
