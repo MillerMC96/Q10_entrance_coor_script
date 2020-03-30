59e083d5bba7aafa5830160a5726f2d783a1c00f
MillerMC96 <pywang@ucdavis.edu>
2020-03-30 15:15:12 -0700

    new run and extracting every half ps

--- extract_frames.sh
+++ extract_frames.sh
@@ -1,37 +1,50 @@
 #!/bin/bash 
 
+#frame buffer
 fb=framebuffer.pdb
+
 #xtc file
 tr=$1
+
 #gromacs
 gro=$2
+
 #coordinate buffer
 cb=$3
 START=0
+
 #frame count
-END=$4
+END=$(($4 * 2))
+#calculating half steps
+#decimal places
+DP="scale=2;"
+
+#time step
+TS="*0.5"
+
 for (( i=$START; i<$END; i++ ))
 do
-    echo 1 | gmx trjconv -f $tr -s $gro -dump $i -o $fb &>/dev/null 
+    TIME=$(echo "$DP$i$TS" | bc)
+    echo 1 | gmx trjconv -f $tr -s $gro -dump $TIME -o $fb &>/dev/null 
     #frame count
     fc=$((i+1))
     echo "frame $fc: " >> $cb
     #SER 66
-    grep '20143' $fb >> $cb
+    grep '14451' $fb >> $cb
     #ALA 63
-    grep '20092\|20093\|20094' $fb >> $cb
+    grep '14397\|14398\|14399' $fb >> $cb
     #PRO 59
-    grep '20039' $fb >> $cb
+    grep '14344' $fb >> $cb
     #ALA 29
-    grep '19649\|19650\|19651' $fb >> $cb
+    grep '13836\|13837\|13838' $fb >> $cb
     #PHE 28
-    grep '19637' $fb >> $cb
+    grep '13824' $fb >> $cb
     #MET 246
-    grep '22898' $fb >> $cb
+    grep '17266' $fb >> $cb
     #LEU 243
-    grep '22850\|22851\|22852' $fb >> $cb
+    grep '17218\|17219\|17220' $fb >> $cb
     #ALA 242
-    grep '22831\|22832\|22833' $fb >> $cb
+    grep '17199\|17200\|17201' $fb >> $cb
     #remove framebuffer
     rm $fb 
 done
